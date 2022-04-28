f32584fc501700ce7aee454dbbb4ce3723d1274f
ste1ns <33740541+ste1ns@users.noreply.github.com>
2022-04-28 07:17:20 +0500

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,1 +1,20 @@
-console.log('Ste1nS test!')
+var TwitchApi = require('twitch-api');
+var twitch = new TwitchApi({
+    clientId: 'your client id',
+    clientSecret: 'your client secret',
+    redirectUri: 'same redirectUri that you have configured on your app',
+    scopes: [array of scopes you want access to]
+  });
+
+twitch.getAccessToken(code, function(err, body){
+    if (err){
+      console.log(err);
+    } else {
+      /*
+      * body = {
+      *   access_token: 'your authenticated user access token',
+      *   scopes: [array of granted scopes]
+      * }
+      */
+    }
+});
